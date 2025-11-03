1448071b3f68de6f3319368751d72e5e76156d63
anish-kapuskar <anishkapuskar@gmail.com>
2025-11-03 15:54:02 +0530

    fix: ECR force_delete

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "this" {
   name                 = var.name
+  force_delete         = true  # fix: auto-delete images when deleting repo
   image_tag_mutability = "MUTABLE"
   tags = var.tags
 }
